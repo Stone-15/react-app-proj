604cb8f83e7c3eea62c383534e3cbac077e8e86d
Aaron <aarg1518@gmail.com>
2024-11-02 20:46:51 +0530

    commit 5

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash  
-# Build Docker image  
-docker-compose build
+# build.sh  
+
+# List all files in the current directory for debugging  
+echo "Current directory contents:"  
+ls -la  
+
+# Run docker-compose build  
+docker-compose build  
+
+# Run docker-compose up to start the services  
+docker-compose up -d
